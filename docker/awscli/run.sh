8cae3bd8cdabda7d9e9e35f108876409a9a8f732
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-06-19 22:57:36 +0900

    add aws-cli

---
+++ docker/awscli/run.sh
@@ -0,0 +1,29 @@
+
+DOTPATH=${DOTPATH:-}
+if [ -z "$DOTPATH" ]; then
+    DOTPATH=$(cd $(dirname $0)/../..; pwd)
+fi
+
+name=$(cd $(dirname $0); basename $(pwd))
+this_docker_path=$DOTPATH/docker/$name
+
+work_dirs=$(echo $(cd $HOME; find * -maxdepth 0 -type d | perl -nle 'print "-v $ENV{HOME}/$_:$ENV{HOME}/$_"'))
+if [ $DOTPATH != $HOME/dotfiles ]; then
+    work_dirs="$work_dirs -v $DOTPATH:$DOTPATH"
+fi
+work_dirs="$work_dirs -v $HOME/.aws:$HOME/.aws"
+
+envs="$envs -e HTTP_PROXY=$HTTP_PROXY -e HTTPS_PROXY=$HTTPS_PROXY -e NO_PROXY=$NO_PROXY"
+envs="$envs -e HOME=$HOME"
+
+user=$(whoami)
+uid=$(id -u $user)
+gid=$(id -g $user)
+if [ -t 0 ]; then
+    term_opt="-it"
+else
+    term_opt=""
+fi
+
+docker run $term_opt --rm -w "$(pwd)" $envs $work_dirs -u $uid:$gid amazon/aws-cli "$@"
+
