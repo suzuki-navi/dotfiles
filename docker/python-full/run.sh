a49f6bdc2051c418246c576b6a7b08839b18fa56
suzuki-navi <suzuki.navi.dev@gmail.com>
2022-07-26 02:29:08 +0900

    various

--- docker/python-full/run.sh
+++ docker/python-full/run.sh
@@ -20,7 +20,7 @@ bash $DOTPATH/docker/lib/build.sh $name >&2
 work_dirs=$(perl $DOTPATH/docker/lib/mount-point-options.pl $DOTPATH $(cd $HOME; ls .))
 
 work_dirs="$work_dirs -v $DOTPATH/credentials/.aws/credentials:$HOME/.aws/credentials"
-work_dirs="$work_dirs -v $DOTPATH/credentials/.aws/config:$HOME/.aws/config"
+work_dirs="$work_dirs -v $DOTPATH/private/.aws/config:$HOME/.aws/config"
 
 docker --config $DOTPATH/.docker/ run --rm $run_options $work_dirs dotfiles-$name bash /var/tmp/lib/entrypoint.sh $name "$@"
 
